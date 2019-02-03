e674850c00e8eac1337121a26467ba22c5ea50d3
Jordi Farre <jordi.farre@mango.com>
2019-02-03 01:34:07 +0100

    create a form without validations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Form, Text } from 'react-form';
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {holderName: '', cardNumber: '', expirationMonth: '', expirationYear: '', cvc: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    if (event.target.id === "holderName") {
+      this.setState({holderName: event.target.value});
+    }
+    if (event.target.id === "cardNumber") {
+      this.setState({cardNumber: event.target.value});
+    }
+    if (event.target.id === "expirationMonth") {
+      this.setState({expirationMonth: event.target.value});
+    }
+    if (event.target.id === "expirationYear") {
+      this.setState({expirationYear: event.target.value});
+    }
+    if (event.target.id === "cvc") {
+      this.setState({cvc: event.target.value});
+    }
+    
+  }
+
+  handleSubmit(event) {
+    console.log('A form was submitted: ' + JSON.stringify(this.state));
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input id="holderName" placeholder="Holder name" type="text" value={this.state.holderName} onChange={this.handleChange} />
+        <input id="cardNumber" placeholder="Card number" type="text" value={this.state.cardNumber} onChange={this.handleChange} />
+        <select id="expirationMonth" value={this.state.expirationMonth} onChange={this.handleChange}>
+          <option value="" selected>Month</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select>
+        <select id="expirationYear" value={this.state.expirationMonth} onChange={this.handleChange}>
+          <option value="" selected>Year</option>
+          <option value="2019">2019</option>
+          <option value="2020">2020</option>
+        </select>
+        <input id="cvc" placeholder="CVC" type="text" value={this.state.cvc} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
