c276633957dbc1778d8999bce15f9aa031763af9
Jordi Farre <jordi.farre@mango.com>
2019-02-03 19:56:18 +0100

    create form using formik

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Form, Text } from 'react-form';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {holderName: '', cardNumber: '', expirationMonth: '', expirationYear: '', cvc: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({[event.target.id]: event.target.value});
-    this.setState({holderNameError: ''});
-    if (!event.target.value) {
-      if (event.target.id === 'holderName') {
-        console.log('error');
-        this.setState({holderNameError: 'HolderName is required'});
-      }
-    }
-  }
-
-  handleSubmit(event) {
-    alert('A form was submitted: ' + JSON.stringify(this.state));
-    event.preventDefault();
-  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input id="holderName" placeholder="Holder name" type="text" value={this.state.holderName} onChange={this.handleChange} />
-        <div>{this.state.holderNameError}</div>
-        <input id="cardNumber" placeholder="Card number" type="text" value={this.state.cardNumber} onChange={this.handleChange} />
-        <select id="expirationMonth" value={this.state.expirationMonth} onChange={this.handleChange}>
-          <option value="">Month</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-        </select>
-        <select id="expirationYear" value={this.state.expirationMonth} onChange={this.handleChange}>
-          <option value="">Year</option>
-          <option value="2019">2019</option>
-          <option value="2020">2020</option>
-        </select>
-        <input id="cvc" placeholder="CVC" type="text" value={this.state.cvc} onChange={this.handleChange} />
-        <input type="submit" value="Submit" />
-      </form>
+      <Formik
+      initialValues={{ holderName: '', cardNumber: '', expirationMonth: '', expirationYear: '', cvc: '' }}
+      validate={values => {
+        let errors = {};
+        if (!values.holderName) {
+          errors.holderName = 'Required';
+        } 
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field name="holderName" placeHolder="Holder Name" />
+          <ErrorMessage name="holderName" component="div" />
+          <Field name="cardNumber" placeHolder="Card Number" />
+          <ErrorMessage name="cardNumber" component="div" />
+          <Field component="select" name="expirationMonth">
+            <option value="">Month</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </Field>
+          <Field component="select" name="expirationMonth">
+            <option value="">Year</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+          </Field>
+          <Field name="cvc" placeHolder="CVC" />
+          <ErrorMessage name="cvc" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
     );
   }
 }
